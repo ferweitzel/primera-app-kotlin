09102c944adf08ad5dc2784ef1d6c996d6b715c2
fernando <fernandoweitzel@gmail.com>
2026-02-17 21:54:27 -0300

    Primer commit de app Kotlin Rama Titulo

--- app/src/main/java/com/example/app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/app_kotlin/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppkotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Fernando",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
